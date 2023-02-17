3f469b16e79c207a44d09b8e5a6e09506289c974
Rdk001 <rmaslenchenko95@gmail.com>
2023-02-17 18:45:24 +0100

    сделал первое задание

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,41 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+function createGalleryElements(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+      <a class="gallery__link"
+        href="${original}"><img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+        /></a>
+        </div> `;
+    })
+    .join("");
+}
+
+const galleryElementsContainerRef = document.querySelector(".gallery");
+const galleryElements = createGalleryElements(galleryItems);
+
+galleryElementsContainerRef.insertAdjacentHTML("beforeend", galleryElements);
+
+galleryElementsContainerRef.addEventListener("click", onGalleryContainerClick);
+
+function onGalleryContainerClick(e) {
+  const isGalleryElement = e.target.classList.contains("gallery__image");
+  if (!isGalleryElement) {
+    return;
+  }
+  e.preventDefault();
+  const originImages = e.target.dataset.source;
+  e.target.src = `${originImages}`;
+  const modal = basicLightbox.create(`
+  <img
+  src="${originImages}"
+  alt="picture"
+  />
+`);
+  modal.show();
+}
