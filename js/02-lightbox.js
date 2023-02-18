a08ae1fd926d0dfcde93b2ad32c54c7347c99e1b
Rdk001 <rmaslenchenko95@gmail.com>
2023-02-18 14:58:10 +0100

    2 задание, не смог сделать задержку

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,32 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+function createGalleryElements(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+      <a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="" title="${description}" />
+    </a>
+        `;
+    })
+    .join("");
+}
 
-console.log(galleryItems);
+const galleryElementsList = document.querySelector(".gallery");
+const galleryElements = createGalleryElements(galleryItems);
+
+galleryElementsList.insertAdjacentHTML("beforeend", galleryElements);
+galleryElementsList.addEventListener("click", onGalleryListClick);
+
+function onGalleryListClick(e) {
+  const isGalleryElement = e.target.classList.contains("gallery__image");
+  if (!isGalleryElement) {
+    return;
+  }
+  e.preventDefault();
+
+  const originImages = e.target.parentElement.href;
+  e.target.src = `${originImages}`;
+}
+
+const lightbox = new SimpleLightbox(".gallery__item", {});
